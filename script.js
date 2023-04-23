7bbf3640c4ede37631ab09d6fe5cf4c2c9f1d980
renkodiranje <renata.tukic@gmail.com>
2023-04-23 13:34:59 +0200

    scroll

--- script.js
+++ script.js
@@ -119,7 +119,12 @@ izracunaj.addEventListener("click", () => {
     ispis1.innerHTML = `Ukupna cena je: ${racun}`;
     document.body.append(ispis1);
   }
-  ispis1.scrollIntoView();
+  red1.scrollIntoView();
+  red2.scrollIntoView();
+  red3.scrollIntoView();
+  red4.scrollIntoView();
+  red5.scrollIntoView();
+  red6.scrollIntoView();
 });
 
 let reset = document.getElementById("resetuj");
